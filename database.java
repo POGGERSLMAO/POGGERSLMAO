e9c0c65ef3b22e27dba9ddcee509330972db60d6
elliot <elliot.palmer@elev.ga.ntig.se>
2020-03-31 11:13:52 +0200

    more code

--- database.java
+++ database.java
@@ -10,7 +10,15 @@ public class database {
                             DatabaseInfo.user, DatabaseInfo.password);
 
            
+            int x = 1;
+            Statement stmt = conn.createStatement();
            
+            String strSelect = "select body from story where id = " + x;
+            String linksSelect = "select target_id, description from links where id = " + x;
+            System.out.println("The SQL statement is: " + strSelect + "\n");
+
+            ResultSet rset = stmt.executeQuery(strSelect);
+
         }
-    }
+    
 }
