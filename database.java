c88c689db3a5b8c77e11784ba306e06d54d33933
elliot <elliot.palmer@elev.ga.ntig.se>
2020-03-31 11:12:29 +0200

    adding code

--- database.java
+++ database.java
@@ -5,53 +5,12 @@ public class database {
         try {
             
             Connection conn = DriverManager.getConnection(
-                    "jdbc:mysql://"+DatabaloginInfo.DBURL + ":" + DatabaloginInfo.port + "/" + DatabaloginInfo.DBname +
+                    "jdbc:mysql://"+DatabaseInfo.DBURL + ":" + DatabaseInfo.port + "/" + DatabaseInfo.DBname +
                             "? allowPublicKeyRetrieval=true&useSSL=false&serverTimezone=UTC",
-                    DatabaseInfo.user, DatabaloginInfo.password);
+                            DatabaseInfo.user, DatabaseInfo.password);
 
            
-            int x = 1;
-            Statement stmt = conn.createStatement();
            
-            String strSelect = "select body from story where id = " + x;
-            String linksSelect = "select target_id, description from links where id = " + x;
-            System.out.println("The SQL statement is: " + strSelect + "\n");
-
-            ResultSet rset = stmt.executeQuery(strSelect);
-
-    
-
-            System.out.println("The records selected are:");
-            while(rset.next()) 
-            
-            {
-
-                String title = rset.getString("body");
-                System.out.println(title);
-            }
-
-            System.out.println("Total number of records = ");
-
-
-
-            ResultSet linkset = stmt.executeQuery(linksSelect);
-
-
-            System.out.println("The records selected are:");
-            int rowCount = 0;
-            while(linkset.next()) {
-                int target_id = linkset.getInt("target_id");
-                String description = linkset.getString("description");
-                System.out.println(target_id + ", " + description);
-                ++rowCount;
-            }
-            System.out.println("Total number of records = " + rowCount);
-
-         
-            conn.close();
-            stmt.close();
-        } catch(SQLException ex) {
-            ex.printStackTrace();
         }
     }
 }
